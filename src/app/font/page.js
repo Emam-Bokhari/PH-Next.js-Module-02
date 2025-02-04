e0882f7351a41446d830f01c701eb89c2ed791c8
Moshfiqur Rahman <studentemam@gmail.com>
2025-02-04 09:18:26 +0600

    feat: implement font optiization

---
+++ src/app/font/page.js
@@ -0,0 +1,14 @@
+import { Fragment } from "react";
+
+export default function FontPage() {
+  return (
+    <Fragment>
+      <p className="text-3xl">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores vero
+        soluta praesentium. Placeat dolorem eaque consequatur itaque harum,
+        porro atque fugiat quas delectus ad? A eligendi sequi quam ipsam
+        recusandae!
+      </p>
+    </Fragment>
+  );
+}
